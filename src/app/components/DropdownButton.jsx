3b023015ed57aa1a6a51d326ca6a27d7dbc5f888
AravArora05 <aravarora05@gmail.com>
2024-07-20 12:49:33 -0400

    Working on mobile responsiveness

---
+++ src/app/components/DropdownButton.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+const DropdownButton = ({ title = "Menu", data }) => {
+  return (
+    <FlyoutLink title={title} FlyoutContent={data} />
+  );
+};
+
+const FlyoutLink = ({ title, FlyoutContent }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const router = useRouter();
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const changeOpenState = () => {
+      setIsOpen(false);
+    };
+
+    router.events.on('routeChangeStart', changeOpenState);
+
+    return () => {
+      router.events.off('routeChangeStart', changeOpenState);
+    };
+  }, []);
+
+  return (
+    <div className="relative inline-block" onClick={toggleDropdown}>
+      <div
+        className="flex justify-center items-center gap-2 cursor-pointer bg-white py-1 px-3 rounded-lg border border-gray-200 shadow-sm hover:bg-gray-100"
+        style={{ width: '120px' }}
+      >
+        <span className="text-sky-900 font-semibold text-lg">
+          {title}
+        </span>
+        {isOpen ? (
+          <FaChevronUp style={{ color: '#6B7280', fontSize: '16px' }} />
+        ) : (
+          <FaChevronDown style={{ color: '#6B7280', fontSize: '16px' }} />
+        )}
+      </div>
+      {isOpen && (
+        <div className="absolute left-0 mt-2 bg-white shadow-lg rounded-lg z-10" style={{ width: '120px' }}>
+          <FlyoutContentComponent options={FlyoutContent} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+const FlyoutContentComponent = ({ options }) => {
+  return (
+    <div className="py-2">
+      {options.map(({ name, link }, index) => (
+        <Link href={link} key={index}>
+          <span className="block hover:bg-gray-100 px-6 py-2 text-gray-900">
+            {name}
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default DropdownButton;
--- src/app/components/Layout.jsx
+++ src/app/components/Layout.jsx
@@ -1,32 +1,69 @@
-// src/components/Layout.js
+"use client";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin, faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin, faFreeCodeCamp } from "@fortawesome/free-brands-svg-icons";
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 import "../globals.css";
+import DropdownButton from "./DropdownButton";
+import { useRouter } from 'next/router';
+
+
 
 const Layout = ({ children }) => {
-  const navItems = ["ABOUT", "RESUME", "PROJECTS", "SKILLS", "CONTACT"];
+  const navItems = [
+    { name: "ABOUT", link: "/about" },
+    { name: "RESUME", link: "/resume" },
+    { name: "PROJECTS", link: "/projects" },
+    { name: "SKILLS", link: "/skills" },
+    { name: "CONTACT", link: "/contact" },
+  ];
 
-  
+  const router = useRouter();
+  const isHomePage = router.pathname === '/';
+
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+
+
   return (
     <main className="flex min-h-screen flex-col bg-blue-50">
-      <header className="flex items-center justify-between bg-white py-2 px-4 border-b border-gray-200 lg:py-2">
-        <h1 className="text-black text-sm lg:text-xl font-semibold tracking-wide">
+      <header className="flex items-center justify-between bg-white py-2 px-4 border-b border-gray-200 lg:py-2.5">
+        <h1 className="text-black text-lg lg:text-xl font-semibold tracking-wide">
           <Link href="/">Arav Arora</Link>
         </h1>
         <nav className="flex text-center">
-          {navItems.map((item, index) => (
-            <Link href={`${item.toLowerCase()}`} key={index}>
-              <span className="text-gray-700 text-xs lg:text-sm uppercase font-medium cursor-pointer hover:text-black px-2 lg:px-4">
-                {item}
-              </span>
-            </Link>
-          ))}
+          {isMobile ? (
+            <DropdownButton title="Menu" data={navItems} />
+          ) : (
+            navItems.map((item, index) => (
+              <Link href={item.link} key={index}>
+                <span className="text-gray-700 text-xs lg:text-sm uppercase font-medium cursor-pointer hover:text-black px-2 lg:px-4">
+                  {item.name}
+                </span>
+              </Link>
+            ))
+          )}
         </nav>
       </header>
 
-      <div className="max-w-7xl mx-auto p-8 mt-6 flex flex-col md:flex-row">
+      <div className={`max-w-7xl mx-auto p-8 mt-6 flex ${isHomePage ? "flex-col" : "flex-col-reverse"} md:flex-row`}>
         <div className="w-full md:w-1/3 pr-8 mb-8 md:mb-0">
           <Image
             className="rounded-full mb-6 border-4 border-blue-200"
@@ -40,7 +77,7 @@ const Layout = ({ children }) => {
             <p className="text-gray-600 mb-2 cursor-pointer">ARAVARORA05@GMAIL.COM</p>
           </Link>
           <div className="flex space-x-4 mb-4">
-            <Link  target="_blank" href="https://github.com/AravArora05">
+            <Link target="_blank" href="https://github.com/AravArora05">
               <span className="hover:text-gray-800 transition duration-300">
                 <FontAwesomeIcon icon={faGithub} size="lg" style={{ color: "#333" }} />
               </span>
@@ -64,17 +101,15 @@ const Layout = ({ children }) => {
               Hello! I'm Arav, a second-year Computer Science student at Georgia Tech, set to graduate in December 2026. I really enjoy working with the MERN stack for full-stack web development. Currently, I'm expanding my knowledge in ML and AI. I'm eager to tackle new challenges and apply my skills to innovative projects in the ever-changing tech industry.
             </p>
             <Link href="/about">
-            <button className="border-2 border-black text-black px-6 py-2 font-semibold hover:bg-blue-100 transition duration-300">
-            ABOUT ME
-            </button>
+              <button className="border-2 border-black text-black px-6 py-2 font-semibold hover:bg-blue-100 transition duration-300">
+                ABOUT ME
+              </button>
             </Link>
           </section>
         </div>
 
-        <div className="w-full md:w-2/3 pl-0 md:pl-8 border-l md:border-l border-gray-300">
-          <div className="w-full bg-white rounded-md px-7 py-6">
-            {children}
-          </div>
+        <div className="w-full md:w-2/3 pl-0 mb-6 md:pl-8 border-l md:border-l border-gray-300">
+          <div className="w-full bg-white rounded-md px-7 py-6">{children}</div>
         </div>
       </div>
     </main>
